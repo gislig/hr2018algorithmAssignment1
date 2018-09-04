19ee129b6e94a776fd09fc2092b55f6dce063ac2
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-09-04 19:39:57 +0000

    Added comments

--- project2.py
+++ project2.py
@@ -1,3 +1,9 @@
+# 1. Checks if the first three numbers are entered
+# 2. If the numbers are above 3 then start sequencing
+# 3. Sums up the first, second and third number into a variable next_num 
+# 4. Each step sets the last number to the next bellow
+# 5. Finally it prints out the next_num
+
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
 first_num = 1
@@ -12,18 +18,25 @@ next_num = 0
 # 6 + 11 + 20 = 37
 
 # This is the formula
+
+# Checks if the n is more or less than one
 if n >= 1:
     print(first_num)
 
+# Checks if the n is more or less than two
 if n >= 2:
     print(second_num)
 
+# Checks if the n is more or less than three
 if n >= 3:
     print(third_num)
 
+# If n is more than three then enter the if loop
 if n > 3:
+    # Sequence the numbers but remove the first three numbers because we have already gone through it
     for seq in range(n-3):
 
+        # Sums up the first, second and third number into next number
         next_num = first_num + second_num + third_num
         first_num = second_num
         second_num = third_num
