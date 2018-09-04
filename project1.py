967b5420ad7d91c9be6cd1374854673d5f7bb00c
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-09-04 18:00:17 +0000

    Bætt inn

--- project1.py
+++ project1.py
@@ -1,1 +1,9 @@
-#Hello PROJECT One
+num_int = 0
+max_int = 0
+
+while num_int > 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    # Fill in the missing code
+    print("The maximum is", max_int)    # Do not change this line
+    if num_int == 0:
+        break
