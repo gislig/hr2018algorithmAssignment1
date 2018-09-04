e7baebaecc3ca5d0acb4fec6c9ec82714a45e005
Aldís Geirdal <aldisgeirdal@gmail.com>
2018-09-04 17:56:38 +0000

    Algorithm test 1

--- project1.py
+++ project1.py
@@ -1,1 +1,16 @@
 #Hello PROJECT One
+
+#Design an algorithm that finds the maximum positive integer input by a user.  
+# The user repeatedly inputs numbers until a negative value is entered.
+
+# 1. Fá jákvæða tölu frá notanda
+# 2. Fá fleiri jákvæðar tölur frá notanda
+# 3. Þegar 
+
+
+num_int = int(input("Input a number: "))    # Do not change this line
+
+
+
+
+print("The maximum is", max_int)    # Do not change this line
