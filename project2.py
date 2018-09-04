cb98f9834992fb6e6bfa0e7d3577d4bcf0a261de
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-09-04 18:40:39 +0000

    Trying to add algorithm for project 2

--- project2.py
+++ project2.py
@@ -1,1 +1,10 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+num_one = 0
+num_two = 0
+num_three = 0
+last_number = 0
+
+# This is the formula
+for seq in range(n):
+    print(seq)
