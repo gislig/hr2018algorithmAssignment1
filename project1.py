006a2e0619819b31f4cfe59b0d12d454d7072098
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-09-04 18:08:05 +0000

    Finished Product

--- project1.py
+++ project1.py
@@ -1,9 +1,15 @@
 num_int = 0
 max_int = 0
 
-while num_int > 0:
+while True:
     num_int = int(input("Input a number: "))    # Do not change this line
+    
     # Fill in the missing code
-    print("The maximum is", max_int)    # Do not change this line
-    if num_int == 0:
+    # Her is the algoritm
+    if num_int > max_int:
+        max_int = num_int
+
+    if num_int < 0:
         break
+
+print("The maximum is", max_int)    # Do not change this line
